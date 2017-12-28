4426f27589f98c11ab97dcd1085711348293f85c
AndreaBiaggi <biaggi.jack@gmail.com>
2017-12-28 16:24:46 +0100

    add assert in testFind

--- src/test/java/org/teamsmarteat/EntityManagerTest.java
+++ src/test/java/org/teamsmarteat/EntityManagerTest.java
@@ -1,6 +1,8 @@
 package org.teamsmarteat;
 
 import junit.framework.TestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.teamsmarteat.model.Menu;
 
 import javax.persistence.EntityManager;
@@ -9,12 +11,15 @@ import javax.persistence.Persistence;
 
 public class EntityManagerTest extends TestCase {
 
-    EntityManager em;
+    private EntityManager em;
+    private static final Logger logger = LogManager.getLogger(EntityManagerTest.class);
 
     public void testFind() {
         EntityManagerFactory factory;
         factory = Persistence.createEntityManagerFactory("unit1");
         em = factory.createEntityManager();
         assertEquals(Menu.class, em.find(Menu.class,1).getClass());
+        assertEquals(String.class, em.find(Menu.class,1).getName().getClass());
+        logger.debug(em.find(Menu.class,1).getName());
     }
 }
