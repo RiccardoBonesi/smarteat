4cdc2dc66ab550ad2ec620efbf974d3d6b87a4b7
AndreaBiaggi <biaggi.jack@gmail.com>
2018-01-07 19:47:07 +0100

    add test for persistence manager

--- src/main/java/org/teamsmarteat/PersistenceManager.java
+++ src/main/java/org/teamsmarteat/PersistenceManager.java
@@ -6,11 +6,9 @@ import javax.persistence.Persistence;
 // Getting the EntityManagerFactory from a singleton in the PersistenceManager class
 public class PersistenceManager {
 
-    public static final boolean DEBUG = true;
-
     private static final PersistenceManager singleton = new PersistenceManager();
 
-    protected EntityManagerFactory emf;
+    private EntityManagerFactory emf;
 
     public static PersistenceManager getInstance() {
 
@@ -21,26 +19,19 @@ public class PersistenceManager {
     }
 
     public EntityManagerFactory getEntityManagerFactory() {
-
         if (emf == null)
             createEntityManagerFactory();
         return emf;
     }
 
     public void closeEntityManagerFactory() {
-
         if (emf != null) {
             emf.close();
             emf = null;
-            if (DEBUG)
-                System.out.println("n*** Persistence finished at " + new java.util.Date());
         }
     }
 
-    protected void createEntityManagerFactory() {
-
-        this.emf = Persistence.createEntityManagerFactory("banana");
-        if (DEBUG)
-            System.out.println("n*** Persistence started at " + new java.util.Date());
+    private void createEntityManagerFactory() {
+        this.emf = Persistence.createEntityManagerFactory("unit1");
     }
 }
