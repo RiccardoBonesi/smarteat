a33e9f1cb8c7e45df4d9fad6f6b23403210a2b33
AndreaBiaggi <biaggi.jack@gmail.com>
2018-01-10 11:09:49 +0100

    add check date and checkout

--- src/main/java/org/teamsmarteat/OrderListAction.java
+++ src/main/java/org/teamsmarteat/OrderListAction.java
@@ -3,16 +3,17 @@ package org.teamsmarteat;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.criterion.Order;
 import org.teamsmarteat.model.OrderEntity;
-import org.teamsmarteat.model.OrderLineEntity;
-import org.teamsmarteat.model.UserEntity;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class OrderListAction extends ActionSupport {
@@ -20,15 +21,21 @@ public class OrderListAction extends ActionSupport {
     private static Logger logger = LogManager.getLogger(OrderListAction.class);
 
     private List<OrderEntity> result;
-    private List<OrderEntity> orders;
+
     public String execute () {
         result = new ArrayList<>();
         EntityManagerFactory factory = PersistenceManager.getInstance().getEntityManagerFactory("unit1");
         EntityManager em = factory.createEntityManager();
         Query query= em.createQuery("SELECT o from OrderEntity o");
-        orders = query.getResultList();
+        List<OrderEntity> orders = query.getResultList();
+        Calendar calendar = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+        today.setTime(new Date());
         for (OrderEntity order: orders) {
-            if (order.getOrderLines().size()!=0)
+            calendar.setTime(order.getDate());
+            boolean sameDay = calendar.get(Calendar.YEAR) == today.get(Calendar.YEAR) &&
+                    calendar.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR);
+            if (order.getOrderLines().size()!=0 && sameDay && !order.isCheckout())
                 result.add(order);
         }
         return SUCCESS;
